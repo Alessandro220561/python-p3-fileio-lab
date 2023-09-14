6c006744ad6dcd7a3de9aceff76c348502591056
Alessandro220561 <124537105+Alessandro220561@users.noreply.github.com>
2023-09-14 10:19:26 -0400

    lab complete

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,43 @@
-def write_file(file_name, file_content):
-    pass
+# def write_file(file_name, file_content):
+#     file_name = file_name + '.txt'
+#     with open(file_name, mode='w', encoding='utf-8') as file:
+#         file.write(file_content)
 
-def append_file(file_name, append_content):
-    pass
+
+# def append_file(file_name, append_content):
+#     file_name = file_name + '.txt'
+#     with open(file_name.txt, mode='a', encoding='utf-8') as file:
+#         file.append(append_content)
+
+
+# def read_file(file_name):
+#     file_name = file_name + '.txt'
+#     open(file_name.txt, encoding='utf-8')
+#     file_name.read()
+
+from pathlib import Path  # Import the pathlib module
+
+
+def write_file(file_name, file_content):
+    if isinstance(file_name, Path):
+        file_name = str(file_name)  # Convert PosixPath to string
+    file_name = file_name + '.txt'  # Add .txt extension to the file_name
+    with open(file_name, mode='w', encoding='utf-8') as file:
+        file.write(file_content)
+
+
+def append_file(file_name, file_content):
+    if isinstance(file_name, Path):
+        file_name = str(file_name)  # Convert PosixPath to string
+    file_name = file_name + '.txt'  # Add .txt extension to the file_name
+    with open(file_name, mode='a', encoding='utf-8') as file:
+        file.write(file_content)
+
 
 def read_file(file_name):
-    pass
+    if isinstance(file_name, Path):
+        file_name = str(file_name)  # Convert PosixPath to string
+    file_name = file_name + '.txt'  # Add .txt extension to the file_name
+    with open(file_name, encoding='utf-8') as file:
+        file_content = file.read()
+    return file_content
